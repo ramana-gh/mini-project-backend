38236e33aff6f6b3383ba9309ae9091570d476ed
Ramanakishore-Peroji <perojiramanakishore@gmail.com>
2021-06-04 10:40:44 +0530

    Cors added

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const routes = require('./routes');
 const validation = require('express-validation');
 require('dotenv').config();
 const PORT = process.env.PORT || '3001';
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/', routes);
